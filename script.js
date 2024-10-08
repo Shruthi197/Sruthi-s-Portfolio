df3b8bed4efd20de8df24710e258bb9c62cdb4bf
Shruthi197 <110449032+Shruthi197@users.noreply.github.com>
2024-10-08 15:17:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Peer Mentor" , "UI/UX Designer" , "Web Developer"],
+    strings: ["Peer Mentor" , "Web Developer", "UI/UX Designer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
